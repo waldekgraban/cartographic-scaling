a5d6774842ff6559385297e3ccb1b812fad654ec
Waldemar Graban <waldekgraban@gmail.com>
2021-08-29 20:28:01 +0200

    Update Calculator.php

--- src/Calculator/Calculator.php
+++ src/Calculator/Calculator.php
@@ -9,29 +9,5 @@ namespace Waldekgraban\CartographicScaling\Calculator;
 
 class Calculator
 {
-    // protected $originalRopeLength;
-    // protected $ropeElasticityConstant;
-    // protected $ropeCrossSection;
-
-    // public function __construct($originalRopeLength, $ropeElasticityConstant, $ropeCrossSection)
-    // {
-    //     $this->originalRopeLength     = $originalRopeLength;
-    //     $this->ropeElasticityConstant = $ropeElasticityConstant;
-    //     $this->ropeCrossSection       = $ropeCrossSection;
-    // }
-
-    // public function getOriginalRopeLength()
-    // {
-    //     return $this->originalRopeLength;
-    // }
-
-    // public function getRopeElasticityConstant()
-    // {
-    //     return $this->ropeElasticityConstant;
-    // }
-
-    // public function getRopeCrossSection()
-    // {
-    //     return $this->ropeCrossSection;
-    // }
+    //
 }
